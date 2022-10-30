386ca63d2889138e630dc3ab124577e1bb8ccd26
noambard1 <noam.bard@yahoo.com>
2022-10-30 17:10:19 +0200

    solved security problem with user sessions

--- backend/frontend/src/auth/LoggedinRoute.js
+++ backend/frontend/src/auth/LoggedinRoute.js
@@ -1,14 +1,28 @@
 import React from 'react'
-import { Route, Redirect } from 'react-router-dom'
+import {Route, Redirect} from 'react-router-dom'
 import {isAuthenticated} from './index';
 
 import LoggedinLayout from "layouts/LoggedinLayout";
 
-const LoggedinRoute = ({ component: Component, ...rest }) => (
+function unitid(){
+  if (isAuthenticated().user.role === "1") {
+    return isAuthenticated().user.gdodid;
+  }
+  if (isAuthenticated().user.role === "2") {
+    return isAuthenticated().user.hativaid;
+  }
+  if (isAuthenticated().user.role === "3") {
+    return isAuthenticated().user.ogdaid;
+  }
+  if (isAuthenticated().user.role === "4") {
+    return isAuthenticated().user.pikodid;
+  }
+}
+const LoggedinRoute = ({props, component: Component, ...rest }) => (
     <Route
         {...rest}
         render ={ props =>
-            isAuthenticated() && (isAuthenticated().user.validated===true)? (
+            isAuthenticated() && (isAuthenticated().user.validated===true) && ((props.match.params.unitid==unitid())||(isAuthenticated().user.role == "0"))  ? (
                 <LoggedinLayout component={Component}/>
             ) : (
                 <Redirect to = {{
