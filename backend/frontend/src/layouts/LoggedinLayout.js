00cfa697a0e19fae4ad4f9008ee0fa01bd1d3e77
GuyMishan <gaiu01999@gmail.com>
2022-12-12 19:58:57 +0200

    removed survey from layout

--- backend/frontend/src/layouts/LoggedinLayout.js
+++ backend/frontend/src/layouts/LoggedinLayout.js
@@ -6,7 +6,7 @@ import { BackgroundColorContext } from "contexts/BackgroundColorContext";
 import { ThemeContext } from "contexts/ThemeContext";
 import BazakNavbar from "components/general/Navbars/BazakNavbar/BazakNavbar";
 //surveys
-import Survey1Modal from 'components/bazak/SurveysModals/Survey1Modal';
+// import Survey1Modal from 'components/bazak/SurveysModals/Survey1Modal';
 
 function LoggedinLayout({ component: Component, ...rest }) {
     // on this page, we need on the body tag the classes .rtl and .menu-on-right
@@ -21,7 +21,7 @@ function LoggedinLayout({ component: Component, ...rest }) {
                         <div className="main-panel">
                             <BazakNavbar theme={theme} />
                             <div className="content" style={{ direction: 'rtl' }}>
-                                <Survey1Modal />
+                                {/* <Survey1Modal /> */}
                                 <Component {...rest} theme={theme} />
                             </div>
                         </div>
